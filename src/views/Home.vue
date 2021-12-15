ac4d6102f155b317a0e80f7a5de4819e396c59bd
kerwinL <kerwin-l@foxmail.com>
2021-12-15 09:38:29 +0800

    :tada: 初次提交

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  created() {
+    console.log(this.$route.params.projectId);
+  }
+};
+</script>
